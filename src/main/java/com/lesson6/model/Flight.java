5e43320774eb1e2faa45a094fa87745967f4bf52
sergiibobykin <4309050@ukr.net>
2019-06-14 13:05:27 +0300

    lesson 6 - CRUD

--- src/main/java/com/lesson6/model/Flight.java
+++ src/main/java/com/lesson6/model/Flight.java
@@ -1,44 +1,110 @@
 package com.lesson6.model;
 
-import lombok.Data;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
 import javax.persistence.*;
 import java.util.*;
 
-@Data
 @Entity
 @Table(name = "FLIGHT")
 public class Flight {
+    private Long id;
+    private Plane plane;
+    private Set<Passenger> passengers = new HashSet<>();
+    private Date dateFlight;
+    private String cityFrom;
+    private String cityTo;
+
+
 
     @Id
     @SequenceGenerator(name = "FL_SEQ", sequenceName = "FLIGHT_SEQ", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FL_SEQ")
     @Column(name = "ID")
-    private Long id;
+    public Long getId() {
+        return id;
+    }
 
-    @ManyToOne (optional=false, fetch=FetchType.LAZY)
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne (optional=false, fetch=FetchType.EAGER)
     @JoinColumn (name="PLANE_ID", nullable=false)
-    private Plane plane;
+    public Plane getPlane() {
+        return plane;
+    }
 
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
 
-    @Transient
-    @ManyToMany
+    @ManyToMany(fetch=FetchType.EAGER)
     @JoinTable(
             name = "FLIGHT_PASSENGER",
-            joinColumns = { @JoinColumn(name = "FLIGHT_ID", referencedColumnName = "ID") },
-            inverseJoinColumns = { @JoinColumn(name = "PASSENGER_ID", referencedColumnName = "ID") }
+            joinColumns = { @JoinColumn(name = "FLIGHT_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "PASSENGER_ID") }
     )
-    private Set<Passenger> passengers = new HashSet<>();
+    public Set<Passenger> getPassengers() {
+        return passengers;
+    }
 
+    public void setPassengers(Set<Passenger> passengers) {
+        this.passengers = passengers;
+    }
 
     @Column(name = "DATE_FLIGHT")
-    private Date dateFlight;
+    public Date getDateFlight() {
+        return dateFlight;
+    }
+
+    public void setDateFlight(Date dateFlight) {
+        this.dateFlight = dateFlight;
+    }
 
     @Column(name = "CITY_FROM")
-    private String cityFrom;
+    public String getCityFrom() {
+        return cityFrom;
+    }
+
+    public void setCityFrom(String cityFrom) {
+        this.cityFrom = cityFrom;
+    }
 
     @Column(name = "CITY_TO")
-    private String cityTo;
+    public String getCityTo() {
+        return cityTo;
+    }
+
+    public void setCityTo(String cityTo) {
+        this.cityTo = cityTo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return Objects.equals(id, flight.id) &&
+                Objects.equals(plane, flight.plane) &&
+                Objects.equals(passengers, flight.passengers) &&
+                Objects.equals(dateFlight, flight.dateFlight) &&
+                Objects.equals(cityFrom, flight.cityFrom) &&
+                Objects.equals(cityTo, flight.cityTo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, plane, passengers, dateFlight, cityFrom, cityTo);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", plane=" + plane +
+                ", passengers=" + passengers +
+                ", dateFlight=" + dateFlight +
+                ", cityFrom='" + cityFrom + '\'' +
+                ", cityTo='" + cityTo + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/lesson6/model/Passenger.java
+++ src/main/java/com/lesson6/model/Passenger.java
@@ -1,41 +1,109 @@
 package com.lesson6.model;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.util.*;
 
-@Data
 @Entity
 @Table(name = "PASSENGER")
 public class Passenger {
+    private Long id;
+    private String lastName;
+    private String nationality;
+    private Date dateOfBirth;
+    private String passportCode;
+    private Set<Flight> flights = new HashSet<>();
+
 
     @Id
     @SequenceGenerator(name = "PS_SEQ", sequenceName = "PASSENGER_SEQ", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PS_SEQ")
     @Column(name = "ID")
-    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     @Column(name = "LAST_NAME")
-    private String lastName;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Column(name = "NATIONALITY")
-    private String nationality;
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 
     @Column(name = "DATE_OF_BIRTH")
-    private Date dateOfBirth;
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 
     @Column(name = "PASSPORT_CODE")
-    private String passportCode;
+    public String getPassportCode() {
+        return passportCode;
+    }
+
+    public void setPassportCode(String passportCode) {
+        this.passportCode = passportCode;
+    }
 
 
-    @Transient
-    @ManyToMany
+    @ManyToMany(fetch=FetchType.EAGER)
     @JoinTable(
-        name = "FLIGHT_PASSENGER",
-        joinColumns = { @JoinColumn(name = "PASSENGER_ID", referencedColumnName = "ID") },
-        inverseJoinColumns = { @JoinColumn(name = "FLIGHT_ID", referencedColumnName = "ID") }
+            name = "FLIGHT_PASSENGER",
+            joinColumns = { @JoinColumn(name = "PASSENGER_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "FLIGHT_ID") }
     )
-    private Set<Flight> flights = new HashSet<>();
+    public Set<Flight> getFlights() {
+        return flights;
+    }
 
+    public void setFlights(Set<Flight> flights) {
+        this.flights = flights;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passenger passenger = (Passenger) o;
+        return Objects.equals(id, passenger.id) &&
+                Objects.equals(lastName, passenger.lastName) &&
+                Objects.equals(nationality, passenger.nationality) &&
+                Objects.equals(dateOfBirth, passenger.dateOfBirth) &&
+                Objects.equals(passportCode, passenger.passportCode) &&
+                Objects.equals(flights, passenger.flights);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lastName, nationality, dateOfBirth, passportCode, flights);
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", passportCode='" + passportCode + '\'' +
+                ", flights=" + flights +
+                '}';
+    }
 }
